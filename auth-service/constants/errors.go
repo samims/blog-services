78494053a4314a4bbb178d96174c1d01991b8248
Samiul Sk <sam91v@gmail.com>
2024-10-28 19:26:44 +0530

    add constants for erros

---
+++ auth-service/constants/errors.go
@@ -0,0 +1,13 @@
+package constants
+
+// Errors
+const (
+	// Token Error messages
+	TokenUnExpectedSigningMethod = "unexpected signing method"
+	TokenInvalidSigningMethod    = "invalid signing method"
+	TokenInvalid                 = "invalid token"
+	TokenExpired                 = "token expired"
+	TokenNotValidYet             = "token is not valid yet"
+	TokenMalformed               = "token is malformed"
+	TokenInvalidIssuer           = "invalid issuer"
+)
